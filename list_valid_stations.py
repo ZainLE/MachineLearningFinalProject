92caea0db2a49db3493f914ef82ffe73092e67dd
ZainLE <latifzain40@gmail.com>
2025-06-06 19:08:22 +0200

    Minor Chnages

--- list_valid_stations.py
+++ list_valid_stations.py
@@ -3,5 +3,5 @@ import pickle
 with open("station_encoder.pkl", "rb") as f:
     encoder = pickle.load(f)
 
-print("✅ Valid station IDs:")
+print(" Valid station IDs:")
 print(list(encoder.classes_))
